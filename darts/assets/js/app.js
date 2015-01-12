a91c8876981e5d6749d1ef98ec4329effe3ec8f7
Andreas Bissinger <mail@bissinger-andreas.de>
2015-01-12 11:08:14 +0100

    Create javascript field for score usability

---
+++ darts/assets/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function (event) {
+	var dart_score = new DartScore();
+	dart_score.okScore();
+	$('div.point_calculator button.point_input_btn.point_number').on('click', function (event) {
+		dart_score.scoreInput($(event.target).attr('data-value'));
+	});
+	$('div.point_calculator button.point_input_btn.point_clear').on('click', function (event) {
+		dart_score.clearScore();
+	});
+	$('div.point_calculator button.point_input_btn.point_ok').on('click', function (event) {
+		dart_score.okScore();
+	});
+});
+
+function DartScore () {
+	var score_window_id = 'score_window';
+	var point_input_id = 'points_input';
+	var score = '0';
+	this.scoreInput = function (number) {
+		if (score.length >= 3 || parseInt(score) == 0) {
+			score = number;
+		} else {
+			score = score + number;
+		}
+		$('#' + score_window_id).html(score);
+	}
+	this.clearScore = function () {
+		score = '0';
+		$('#' + score_window_id).html(score);
+	};
+	this.okScore = function () {
+		$('#' + point_input_id).val(parseInt(score));
+	};
+}
